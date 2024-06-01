33b4829da18678101ca9bfbe739b81be8a8116e3
Autsada555 <autsadaheha@gmail.com>
2024-06-01 23:18:58 +0700

    FirstCommit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from "@/components/ui/button"
+import  Navbar  from './pages/customer/navbar'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+      <Button>Click me!!!!!!!!</Button>
+    </>
+  )
+}
+
+export default App
