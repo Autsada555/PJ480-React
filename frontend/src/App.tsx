e91c8ffe2ba69daa500b8227e4deb970acdce971
Autsada555 <autsadaheha@gmail.com>
2024-07-08 19:30:39 +0700

    updateApp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
 import './App.css'
-import React from 'react';
 
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
@@ -8,8 +6,10 @@ import { Route, Routes } from "react-router-dom";
 import './App.css';
 import { Customer} from './pages/admin/Customer';
 
+
 function App() {
   return (
+  <div>
     <BrowserRouter>
       <div className="router ">
         <Routes>
@@ -19,6 +19,7 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
+    </div>
   );
 }
 
