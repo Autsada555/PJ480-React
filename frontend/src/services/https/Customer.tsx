6c9bee8aecd9480ad99acef7d6b4f907fff8faab
Autsada555 <autsadaheha@gmail.com>
2024-08-24 15:56:40 +0700

    updateListuser

--- frontend/src/services/https/Customer.tsx
+++ frontend/src/services/https/Customer.tsx
@@ -5,7 +5,7 @@ const apiUrl = "http://localhost:8080";
 
 //
 const GetAllCustomer = async () => {
-  const requestOptions :RequestInit= {
+  const requestOptions: RequestInit = {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -28,127 +28,130 @@ const GetAllCustomer = async () => {
 }
 
 const GetCustomer = async (id: number) => {
-    const requestOptions :RequestInit=  {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-  
-    let res = await fetch(`${apiUrl}/customer/${id}`, requestOptions)
-      .then((response) => response.json())
-      .then((res) => {
-        if (res.data) {
-          return res.data;
-        } else {
-          return false;
-        }
-      });
-  
-    return res;
-  }
+  const requestOptions: RequestInit = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include"
+  };
 
-  async function CreateCustomer(formData: UserFormData){
-    const requestOptions :RequestInit=  {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formData),
-    };
-  
-    let res = await fetch(`${apiUrl}/customer/create`, requestOptions)
-      .then((response) => response.json())
-      .then((res) => {
-        if (res.data) {
-          return { status: true, message: res.data };
-        } else {
-          return { status: false, message: res.error };
-        }
-      });
-  
-    return res;
-  }
+  let res = await fetch(`${apiUrl}/customer/${id}`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data;
+      } else {
+        return false;
+      }
+    });
 
-  async function UpdateCustomer(formData: UserUpdateFormData) {
-    const requestOptions :RequestInit= {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formData),
-    };
-  
-    let res = await fetch(`${apiUrl}/customer/edit`, requestOptions)
-      .then((response) => response.json())
-      .then((res) => {
-        if (res.data) {
-          return { status: true, message: res.data };
-        } else {
-          return { status: false, message: res.error };
-        }
-      });
-  
-    return res;
-    } 
+  return res;
+}
 
-  async function DeleteCustomer(id: number | undefined) {
-    const requestOptions :RequestInit=  {
-      method: "DELETE"
-    };
-    let res = await fetch(`${apiUrl}/customer/delete/${id}`, requestOptions)
-      .then((response) => response.json())
-      .then((res) => {
-  
-        if (res.message) {
-          return { status: true, message: res.message };
-        } else {
-          return { status: false, message: res.error };
-        }
-      });
-  
-    return res;  
-  }
+async function CreateCustomer(formData: UserFormData) {
+  const requestOptions: RequestInit = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify(formData),
+  };
 
-  const GetAllGender = async () => {
-    const requestOptions :RequestInit= {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include"
-  
-    };
-  
-    let res = await fetch(`${apiUrl}/customer/gender`, requestOptions)
-      .then((response) => response.json())
-      .then((res) => {
-        if (res.data) {
-          return res.data;
-        } else {
-          return false;
-        }
-      });
-  
-    return res;
-  }
+  let res = await fetch(`${apiUrl}/customer/create`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return { status: true, message: res.data };
+      } else {
+        return { status: false, message: res.error };
+      }
+    });
 
-  const GetAllUserType = async () => {
-    const requestOptions :RequestInit= {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include"
-  
-    };
-  
-    let res = await fetch(`${apiUrl}/customer/usertype`, requestOptions)
-      .then((response) => response.json())
-      .then((res) => {
-        if (res.data) {
-          return res.data;
-        } else {
-          return false;
-        }
-      });
-  
-    return res;
-  }
-export { GetCustomer,  UpdateCustomer, DeleteCustomer, GetAllCustomer, CreateCustomer, GetAllGender, GetAllUserType }
+  return res;
+}
+
+async function UpdateCustomer(formData: UserUpdateFormData) {
+  const requestOptions: RequestInit = {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+    credentials: "include"
+  };
+
+  let res = await fetch(`${apiUrl}/customer/edit`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return { status: true, message: res.data };
+      } else {
+        return { status: false, message: res.error };
+      }
+    });
+
+  return res;
+}
+
+async function DeleteCustomer(id: number | undefined) {
+  const requestOptions: RequestInit = {
+    method: "DELETE"
+  };
+  let res = await fetch(`${apiUrl}/customer/delete/${id}`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+
+      if (res.message) {
+        return { status: true, message: res.message };
+      } else {
+        return { status: false, message: res.error };
+      }
+    });
+
+  return res;
+}
+
+const GetAllGender = async () => {
+  const requestOptions: RequestInit = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include"
+
+  };
+
+  let res = await fetch(`${apiUrl}/customer/gender`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+  return res;
+}
+
+const GetAllUserType = async () => {
+  const requestOptions: RequestInit = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include"
+
+  };
+
+  let res = await fetch(`${apiUrl}/customer/usertype`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+  return res;
+}
+export { GetCustomer, UpdateCustomer, DeleteCustomer, GetAllCustomer, CreateCustomer, GetAllGender, GetAllUserType }
